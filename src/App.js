6d465f61137aa53401de91c83b45db9ac092b1d4
Kushagra Agrawal <agrawalkushagra29@gmail.com>
2024-01-05 21:35:55 +0530

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Layout from './components/Layout';
 import Loader from './components/Loader';
 import Main from './components/Main';
 import Quiz from './components/Quiz/Quiz';
+import Result from './components/Result/Result';
+import shuffle from './components/utils';
 
 function App() {
   const [loading,setLoading] = useState(false);
@@ -44,6 +46,44 @@ function App() {
     },2000);
   };
 
+  const replayQuiz = () => {
+    setLoading(true);
+    setLoadingMessage({
+      title:"Getting ready for round two.",
+      message:"It won't take long!."
+    })
+
+    const shuffleData = shuffle(data);
+    shuffleData.forEach(element => {
+      element.options = shuffle(element.options);
+    });
+
+    setData(shuffleData);
+
+    setTimeout(()=>{
+      setIsQuizStarted(true);
+      setIsQuizCompleted(false);
+      setResultData(null);
+      setLoading(false);
+    },1000)
+  }
+
+  const resetQuiz = () => {
+    setLoading(true);
+    setLoadingMessage({
+      title:"Loading to your Home Screen.",
+      message:"Thank you for applying!."
+    });
+
+    setTimeout(() => {
+      setData(null);
+      setCountdownTime(null);
+      setIsQuizStarted(false);
+      setIsQuizCompleted(false);
+      setResultData(null);
+      setLoading(false);
+    },1000);
+  };
 
   return (
     <Layout>
@@ -53,6 +93,9 @@ function App() {
       )}
       {!loading && isQuizStarted && (
         <Quiz data={data} countdownTime={countdownTime} endQuiz={endQuiz}/>
+      )}
+      {!loading && isQuizCompleted && (
+        <Result {...resultData} replayQuiz={replayQuiz} resetQuiz={resetQuiz}/>
       )}
     </Layout>
   );
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,11 +1,151 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Container,
+  Divider,
+  Header,
+  Icon,
+  Item,
+  Menu,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import Countdown from "../Countdown";
+import he from "he";
+import { getLetter } from "../utils";
 
-const Quiz = () => {
+const Quiz = ({ data, countdownTime, endQuiz }) => {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [timeTaken, setTimeTaken] = useState(null);
+  const [userSelectedAns, setUserSelectedAns] = useState(null);
+  const [correctAnswers, setCorrectedAnswers] = useState(0);
+  const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
+
+  useEffect(() => {
+    if (questionIndex > 0) {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  }, [questionIndex]);
+
+  const handleItemClick = (e, { name }) => {
+    setUserSelectedAns(name);
+  };
+
+  const handleNext = () => {
+    let point = 0;
+
+    if (userSelectedAns === he.decode([questionIndex].correct_answer)) {
+      point = 1;
+    }
+
+    const qna = questionsAndAnswers;
+    qna.push({
+      question: he.decode(data[questionIndex].question),
+      user_answer: userSelectedAns,
+      correct_answer: he.decode(data[questionIndex].correct_answer),
+      point,
+    });
+
+    if (questionIndex === data.length - 1) {
+      return endQuiz({
+        totalQuestions: data.length,
+        correctAnswers: correctAnswers + point,
+        timeTaken,
+        questionsAndAnswers: qna,
+      });
+    }
+
+    setCorrectedAnswers(correctAnswers + point);
+    setQuestionIndex(questionIndex + 1);
+    setUserSelectedAns(null);
+    setQuestionsAndAnswers(qna);
+  };
+
+  const timeOver = (timeTaken) => {
+    return endQuiz({
+      totalQuestions: data.length,
+      correctAnswers,
+      timeTaken,
+      questionsAndAnswers,
+    });
+  };
+
   return (
-    <div>
-      Quiz
-    </div>
-  )
-}
+    <Item.Header>
+      <Container>
+        <Segment>
+          <Item.Group divided>
+            <Item>
+              <Item.Content>
+                <Item.Extra>
+                  <Header as="h1" block floated="left">
+                    <Icon name="info-circle" />
+                    <Header.Content>
+                      {`Question No.${questionIndex + 1} of ${data.length}`}
+                    </Header.Content>
+                  </Header>
+                  <Countdown
+                    countdownTime={countdownTime}
+                    timeOver={timeOver}
+                    setTimeTaken={setTimeTaken}
+                  />
+                </Item.Extra>
+                <br />
+                <Item.Meta>
+                  <Message size="huge" floating>
+                    <b>{`Q. ${he.decode(data[questionIndex].question)}`}</b>
+                  </Message>
+                  <br />
+                  <Item.Description>
+                    <p>Please chooose one of the following answers:</p>
+                  </Item.Description>
+                  <Divider />
+                  <Menu vertical fluid size="massive">
+                    {data[questionIndex].options.map((option, index) => {
+                      const letter = getLetter(index);
+                      const decodeOption = he.decode(option);
 
-export default Quiz
+                      return (
+                        <Menu.Item
+                          key={decodeOption}
+                          name={decodeOption}
+                          active={userSelectedAns === decodeOption}
+                          onClick={handleItemClick}
+                        >
+                          <b style={{ marginRight: "8px" }}>{letter}</b>
+                          {decodeOption}
+                        </Menu.Item>
+                      );
+                    })}
+                  </Menu>
+                </Item.Meta>
+                <Divider />
+                <Item.Extra>
+                  <Button
+                    primary
+                    content="Next"
+                    onClick={handleNext}
+                    floated="right"
+                    size="big"
+                    icon="right chevron"
+                    disabled={!userSelectedAns}
+                  />
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+        <br />
+      </Container>
+    </Item.Header>
+  );
+};
+
+Quiz.propTypes = {
+  data: PropTypes.array.isRequired,
+  countdownTime: PropTypes.number.isRequired,
+  endQuiz: PropTypes.func.isRequired,
+};
+
+export default Quiz;
